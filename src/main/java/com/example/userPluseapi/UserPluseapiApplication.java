5e24b03a965ad6a63b012dc964f2d7eefa830a33
rakesh <csrakesh32@gmail.com>
2025-06-16 07:27:31 +0530

    first commit

---
+++ src/main/java/com/example/userPluseapi/UserPluseapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.userPluseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPluseapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPluseapiApplication.class, args);
+	}
+
+}
